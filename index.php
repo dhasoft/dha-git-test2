59b591a5c1f9914ee4d8ce5de2429e75003b685e
dhasoft <wardhana.wr@gmail.com>
2024-07-17 18:12:03 +0700

    tambah baris ul

--- index.php
+++ index.php
@@ -13,6 +13,11 @@
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam eius voluptatem et perspiciatis architecto natus
         facilis veritatis repellendus, totam quisquam dignissimos qui ad dolore cupiditate aut minima repellat,
         exercitationem maiores?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
 </body>
 
 </html>
